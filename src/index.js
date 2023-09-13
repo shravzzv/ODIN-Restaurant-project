eb7ef127b23508a44c654adeeb38658d074587b9
sai shravan <saishravan384@gmail.com>
2023-09-13 14:41:57 +0530

    build: Update index.js and build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles/style.css'
-import homePage from './homePage'
+import homePage from './pages/homePage'
 
 const content = document.querySelector('#content')
 content.appendChild(homePage())
